28e8dbc346228b06f79356f755c0ac56e6bfac44
HLin666 <1981466501@qq.com>
2025-05-15 15:19:03 +0800

    新增属性和子属性的抽象类以及一些属性实现类

---
+++ attribute_structures.py
@@ -0,0 +1,181 @@
+from data_structures import Attribute, SubAttribute
+
+class Slope(Attribute):
+    """坡度属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class Exposure(Attribute):
+    """坡向属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class Curvature(Attribute):
+    """曲率属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class Roughness(Attribute):
+    """地形粗糙度属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class Relief(Attribute):
+    """地形起伏度属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class ElevationCoefficientOfVariation(Attribute):
+    """高程变异系数属性"""
+    def __init__(self, value):
+        super().__init__(value)
+
+class Water(Attribute):
+    """
+    水体属性
+    """
+    class WaterDepth(SubAttribute):
+        """水深子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    class WaterBottomGeology(SubAttribute):
+        """水底地质子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    class FlowSpeed(SubAttribute):
+        """流速子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    class WaterWidth(SubAttribute):
+        """水体宽度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    class BankSteepness(SubAttribute):
+        """岸边陡峭程度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    def __init__(self, value):
+        super().__init__(value)
+
+class Vegetation(Attribute):
+    """
+    植被属性
+    """
+    class VegetationType(SubAttribute):
+        """植被类型子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class VegetationDensity(SubAttribute):
+        """植被密度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class AverageVegetationHeight(SubAttribute):
+        """平均植被高度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class AveragePlantDiameter(SubAttribute):
+        """平均植株胸径子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class CanopyClosure(SubAttribute):
+        """林冠层郁闭度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    def __init__(self, value):
+        super().__init__(value)
+
+class Soil(Attribute):
+    """
+    土壤属性
+    """
+    class SoilType(SubAttribute):
+        """土壤类型子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class SoilHardness(SubAttribute):
+        """土壤硬度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class SoilBearingCapacity(SubAttribute):
+        """土壤承载力子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    class SurfaceSoilThickness(SubAttribute):
+        """表层土厚度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    def __init__(self, value):
+        super().__init__(value)
+
+class Building(Attribute):
+    """
+    建筑物属性
+    """
+    class BuildingType(SubAttribute):
+        """建筑物类型子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    class BuildingHardness(SubAttribute):
+        """建筑物硬度子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+    
+    class BuildingDestructibility(SubAttribute):
+        """建筑物可破坏性子属性"""
+        def __init__(self, value):
+            super().__init__(value)
+
+    def __init__(self, value):
+        super().__init__(value)
+
+if __name__ == '__main__':
+    # 水体属性示例代码
+    water = Water("水体")
+    water_depth = Water.WaterDepth(5.0)
+    water_bottom_geology = Water.WaterBottomGeology("沙质底")
+    flow_speed = Water.FlowSpeed(1.5)
+    water_width = Water.WaterWidth(10.0)
+    bank_steepness = Water.BankSteepness("陡峭")
+    water.add_sub_attribute(water_depth)
+    water.add_sub_attribute(water_bottom_geology)
+    water.add_sub_attribute(flow_speed)
+    water.add_sub_attribute(water_width)
+    water.add_sub_attribute(bank_steepness)
+    print(f"水体属性: {water.get_value()}")
+    print(f"水深: {water.get_sub_attribute(Water.WaterDepth).get_value()}")
+    print(f"水底地质: {water.get_sub_attribute(Water.WaterBottomGeology).get_value()}")
+    print(f"流速: {water.get_sub_attribute(Water.FlowSpeed).get_value()}")
+    print(f"水体宽度: {water.get_sub_attribute(Water.WaterWidth).get_value()}")
+    print(f"岸边陡峭程度: {water.get_sub_attribute(Water.BankSteepness).get_value()}")
+    # 植被属性示例代码
+    vegetation = Vegetation("植被")
+    vegetation_type = Vegetation.VegetationType("阔叶林")
+    vegetation_density = Vegetation.VegetationDensity(0.8)
+    average_vegetation_height = Vegetation.AverageVegetationHeight(15.0)
+    average_plant_diameter = Vegetation.AveragePlantDiameter(0.3)
+    canopy_closure = Vegetation.CanopyClosure(0.7)
+    vegetation.add_sub_attribute(vegetation_type)
+    vegetation.add_sub_attribute(vegetation_density)
+    vegetation.add_sub_attribute(average_vegetation_height)
+    vegetation.add_sub_attribute(average_plant_diameter)
+    vegetation.add_sub_attribute(canopy_closure)
+    print(f"植被属性: {vegetation.get_value()}")
+    print(f"植被类型: {vegetation.get_sub_attribute(Vegetation.VegetationType).get_value()}")
+    print(f"植被密度: {vegetation.get_sub_attribute(Vegetation.VegetationDensity).get_value()}")
+    print(f"平均植被高度: {vegetation.get_sub_attribute(Vegetation.AverageVegetationHeight).get_value()}")
+    print(f"平均植株胸径: {vegetation.get_sub_attribute(Vegetation.AveragePlantDiameter).get_value()}")
+    print(f"林冠层郁闭度: {vegetation.get_sub_attribute(Vegetation.CanopyClosure).get_value()}")
+    
--- data_structures.py
+++ data_structures.py
@@ -1,4 +1,5 @@
 import h3
+from abc import ABC, abstractmethod
 
 class Map:
     def __init__(self):
@@ -29,11 +30,14 @@ class Cell:
         self.highway_edges = []       # 公路通行边数组 (bool列表，索引对应neighbors)
         self.has_highway = False      # 是否包含公路
 
-        # 栅格量化属性
+        # dem量化属性
         self.elevation = None         # 高程值
         self.slope = None             # 坡度值
         self.exposure = None          # 坡向值
         self.terrain = {}             # 地形类型
+
+        # 其他属性
+        self.attribute = []           # 属性数组 (存储Attribute对象)
 
     def init_center(self):
         """
@@ -57,3 +61,40 @@ class Cell:
         neighbor_indexes.discard(self.h3_index)  # 移除自身索引
         self.neighbors = [index for index in neighbor_indexes]  # 填充邻接cell的索引
         
+# 属性抽象类
+class Attribute(ABC):
+    def __init__(self, value):
+        self.value = value
+        self.sub_attribute = []
+
+    def add_sub_attribute(self, sub_attribute):
+        """
+        添加子属性
+        """
+        self.sub_attribute.append(sub_attribute)
+
+    def get_value(self):
+        """
+        获取当前属性值
+        """
+        return self.value
+    
+    def get_sub_attribute(self, target_class):
+        """
+        获取子属性
+        """
+        for sub in self.sub_attribute:
+            if isinstance(sub, target_class):
+                return sub
+        return None
+    
+# 子属性抽象类
+class SubAttribute(ABC):
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        """
+        获取当前子属性值
+        """
+        return self.value
